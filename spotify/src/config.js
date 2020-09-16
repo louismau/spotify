2813aec95a35019e09fc0ae308960d3fc6854e96
Louis Mauriac <mauriaclouis@hotmail.com>
2020-09-16 14:33:50 +0200

    change redirection

--- spotify/src/config.js
+++ spotify/src/config.js
@@ -1,6 +1,6 @@
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const clientId = "176a8a490c1640438983dd07875fec7c";
-const redirectUri = "https://cover-quizz.herokuapp.com/callbacks";
+const redirectUri = "http://localhost:3000/callback";
 const scopes = [
   'user-top-read',
 ];
